1a0bcba24aced3bb6b439d1619867c5164a5daf1
Erin Morrissey <erinmorrissey@gmail.com>
2016-06-13 22:02:04 +0000

    Lockdown Place edit/update access to current user only

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all.order(:id).paginate(:page => params[:page], :per_page => 1)
@@ -20,11 +20,21 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   # gets executed when user presses the button on the Edit Place form
+  # the RETURN in the if statement prevents the rest of the code from
+  #   running in the event that the wrong user triggered an update
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
