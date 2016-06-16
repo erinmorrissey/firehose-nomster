0f6b42c66bbe76cc5e5c9383f3966b8f6a7f4b10
Erin Morrissey <erinmorrissey@gmail.com>
2016-06-16 16:17:11 +0000

    Configure SendGrid for email sending from production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Nomster::Application.configure do
 
   # added for Devise configurations
   config.action_mailer.default_url_options = { host: 'firehose-nomster.herokuapp.com' }
+
+  # SendGrid configurations for sending emails
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+    }
 end
