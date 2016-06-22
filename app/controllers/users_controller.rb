5af70f84cc6b13514d16d4b66a38f5c4d2f84909
Erin Morrissey <erinmorrissey@gmail.com>
2016-06-22 22:37:02 +0000

    Add @user data to Users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
 
   def show
+    # takes the user id from the URL, looks up the user in the DB, and puts the value into @user,
+    # which we can then use inside of our view
+    @user = User.find(params[:id])
   end
-  
+
 end
