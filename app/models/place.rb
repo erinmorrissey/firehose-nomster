35c07141a4771976ca76716c03ef5a9c67b6c98f
Erin Morrissey <erinmorrissey@gmail.com>
2016-06-25 01:01:55 +0000

    Add dependent destroy to comments association on 'place' model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,9 @@ class Place < ActiveRecord::Base
   # belongs_to means links_to another table (each Place links_to another user)
   belongs_to :user
   # has_many means links_from another table (each Place has a link from a comment)
-  has_many :comments
+  # dependent destroy means the comments connected to a place will be 
+  #    automatically destroyed with the place itself is removed
+  has_many :comments, :dependent => :destroy
   has_many :photos
   
   geocoded_by :address
